97222cb36d716f1cfdb5a6206bdf15d16fc46dfd
David Waterman <dgwaterman@gmail.com>
2021-05-13 00:29:52 +0100

    Working version

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-# Run with sudo
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-cat > /usr/share/applications/rclone_pasaffe.desktop <<+
+DESKTOP_FILE=$HOME/.local/share/applications/rclone_pasaffe.desktop
+cat > $DESKTOP_FILE <<+
 [Desktop Entry]
 Name=Remote Pasaffe
 Name[en_GB]=Remote Pasaffe
 Comment[en_GB]=Pasaffe password manager with rclone wrapping
 Categories=GNOME;Utility;
-Exec=$SCRIPT_DIR/remote_pasaffe.sh %f
+Exec=$SCRIPT_DIR/rclone_pasaffe.sh %f
 Icon=$SCRIPT_DIR/pwsafe.png
 Terminal=false
 Type=Application
-MimeType=application/x-psafe3
 +
+chmod +x $DESKTOP_FILE
